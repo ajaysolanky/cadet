88d9f98010ca32ba14c8aa73edd330263937888b
Ajay Solanky <ajaysolanky@Ravis-MacBook-Pro-2.local>
2023-03-13 15:19:34 -0400

    added pymupdf python dependency and got rid of unnecessary CORS stuff

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ from OpenAIQuery import OpenAIQuery
 from utils import GhettoDiskCache
 
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app)
 
 @app.route('/getjd', methods=['GET'])
 def get_jd():
